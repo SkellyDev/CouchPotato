ca964e98f65be800d21ef8934896a9d629b54559
SkellyDev <SkellyDev11250512@gmail.com>
2020-11-04 20:15:03 -0800

    Edit Mission.py

--- src/Mission.py
+++ src/Mission.py
@@ -9,25 +9,30 @@ class Mission:
         self._SIZE = 60
         self._DENSITY = 0.001
 
-    def spawn_type(self, type):
+    def spawn_type(self, type_list):
         result_xml = ""
         for _ in range(int((self._SIZE*2)**2*self._DENSITY)):
             xd, zd = random.randint(
                 0, self._SIZE), random.randint(0, self._SIZE)
+            type = random.choice(type_list)
             result_xml += "<DrawEntity x='{}' y='5' z='{}' type='{}'/>".format(
                 xd, zd, type)
         return result_xml
 
     def draw_tree(self, x, z):
-        leave_density = 1
         result_xml = ""
         for l in range(4, 9):
             result_xml += f"<DrawBlock x='{x}'  y='{l}' z='{z}' type='dirt' />"
         # Draw leaves
+        leave_density = 0.9
         for i, l in zip([2, 2, 1], range(7, 10)):
+            pos_list = []
             for _ in range(int((2*i+1)**2*leave_density)):
                 xd, zd = random.randint(x-i, x+i), random.randint(z-i, z+i)
+                while (xd, zd) in pos_list:
+                    xd, zd = random.randint(x-i, x+i), random.randint(z-i, z+i)
                 result_xml += f"<DrawBlock x='{xd}'  y='{l}' z='{zd}' type='leaves' />"
+                pos_list.append((xd, zd))
             leave_density -= .1
         return result_xml
 
@@ -86,19 +91,39 @@ class Mission:
         # Draw flowers
         flower_list = ["yellow_flower", "red_flower",
                        "brown_mushroom", "red_mushroom"]
-        for i in range(int((x2-x1+2)*0.9)):
-            xd = random.choice(list(i for i in range(
+        xd_list1 = []
+        xd_list2 = []
+        for i in range(int((x2-x1+2)*0.7)):
+            xd1 = random.choice(list(i for i in range(
                 x1-1, x2+1) if i not in [x1+3, x1+4]))
-            result_xml += f"<DrawBlock x='{xd}'  y='4' z='{z1-1}' type='{random.choice(flower_list)}' />"
-            xd = random.choice(list(i for i in range(
-                x1-1, x2+1) if i not in [x1+3, x1+4]))
-            result_xml += f"<DrawBlock x='{xd}'  y='4' z='{z2+1}' type='{random.choice(flower_list)}' />"
+            while xd1 in xd_list1:
+                xd1 = random.choice(list(i for i in range(
+                    x1-1, x2+1) if i not in [x1+3, x1+4]))
+            result_xml += f"<DrawBlock x='{xd1}'  y='4' z='{z1-1}' type='{random.choice(flower_list)}' />"
+            xd_list1.append(xd1)
 
-        for i in range(int((z2-z1+2)*0.9)):
-            zd = random.randint(z1-1, z2+1)
-            result_xml += f"<DrawBlock x='{x1-1}'  y='4' z='{zd}' type='{random.choice(flower_list)}' />"
-            zd = random.randint(z1-1, z2+1)
-            result_xml += f"<DrawBlock x='{x2+1}'  y='4' z='{zd}' type='{random.choice(flower_list)}' />"
+            xd2 = random.choice(list(i for i in range(
+                x1-1, x2+1) if i not in [x1+3, x1+4]))
+            while xd2 in xd_list2:
+                xd2 = random.choice(list(i for i in range(
+                    x1-1, x2+1) if i not in [x1+3, x1+4]))
+            result_xml += f"<DrawBlock x='{xd2}'  y='4' z='{z2+1}' type='{random.choice(flower_list)}' />"
+            xd_list2.append(xd2)
+
+        zd_list1 = []
+        zd_list2 = []
+        for i in range(int((z2-z1+2)*0.7)):
+            zd1 = random.randint(z1-1, z2+1)
+            while zd1 in zd_list1:
+                zd1 = random.randint(z1-1, z2+1)
+            result_xml += f"<DrawBlock x='{x1-1}'  y='4' z='{zd1}' type='{random.choice(flower_list)}' />"
+            zd_list1.append(zd1)
+
+            zd2 = random.randint(z1-1, z2+1)
+            while zd2 in zd_list1:
+                zd2 = random.randint(z1-1, z2+1)
+            result_xml += f"<DrawBlock x='{x2+1}'  y='4' z='{zd2}' type='{random.choice(flower_list)}' />"
+            zd_list2.append(zd2)
 
         return result_xml
 
@@ -126,8 +151,9 @@ class Mission:
                 <DrawingDecorator>''' +  \
             "<DrawCuboid x1='{}' x2='{}' y1='4' y2='4' z1='{}' z2='{}' type='air'/>".format(0, self._SIZE, 0, self._SIZE) + \
             "<DrawCuboid x1='{}' x2='{}' y1='3' y2='3' z1='{}' z2='{}' type='grass'/>".format(0, self._SIZE, 0, self._SIZE) + \
-            self.spawn_type('Pig') + self.draw_wall(0, self._SIZE, 0, self._SIZE) + self.draw_house(30, 40, 30, 40) + self.draw_tree(20, 41) + \
-            self.draw_tree(7, 13) + self.draw_tree(43, 17) + self.draw_tree(38, 11) + self.draw_lake(15, 25, 10, 20) + \
+            self.spawn_type(['Pig', 'Cow', 'Sheep']) + self.draw_wall(0, self._SIZE, 0, self._SIZE) + self.draw_house(30, 40, 30, 40) + self.draw_tree(20, 41) + \
+            self.draw_tree(7, 13) + self.draw_tree(43, 17) + self.draw_tree(38, 11) + self.draw_tree(7, 50) + \
+            self.draw_lake(15, 25, 10, 20) + \
             '''</DrawingDecorator>
                 <ServerQuitWhenAnyAgentFinishes/>
             </ServerHandlers>
